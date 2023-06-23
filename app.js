01a2e417b93e2d57e3a3d41d818e55e7a5816511
RafaelCRC <rafael.cardoso@liferay.com>
2023-06-22 21:59:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+
+// var tests = ["test01", "test02"]
+
+// if (tests.includes("test03")){
+//     console.log(tests, typeof(tests))
+// } else if (typeof(tests) == "object"){
+//     console.log(tests[1])
+// }
+
+// for (i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// for (a in tests) {
+//     console.log(tests[a])
+// }
