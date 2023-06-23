ef5b36e37049c247b0c7e3f370cf1176a60bc7e2
RafaelCRC <rafael.cardoso@liferay.com>
2023-06-23 16:09:42 -0300

    add products API documentation

--- routes/products.js
+++ routes/products.js
@@ -10,8 +10,23 @@ router.get('/', (req, res, next) => {
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error })}
-
-                return res.status(200).send({ response: result});
+                const response = {
+                    quantity: result.length,
+                    products: result.map(prod => {
+                        return {
+                            id_product: prod.id_product,
+                            name_product: prod.name_product,
+                            price_product: prod.price_product,
+                            request: {
+                                type: 'GET',
+                                description: 'Return product details',
+                                url: 'http://localhost:3000/products/' + prod.id_product
+                            }
+                        }
+                    })
+                }
+                
+                return res.status(200).send(response);
             }
         );
     });
@@ -26,8 +41,26 @@ router.get('/:id_product', (req, res, next) => {
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error })}
+                
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        message: 'Product not found'
+                    });
+                }
+                const response = {
+                    product: {
+                        id_product: result[0].id_product,
+                        name_product: result[0].name_product,
+                        price_product: result[0].price_product,
+                        request: {
+                            type: 'GET',
+                            description: 'Return all products',
+                            url: 'http://localhost:3000/products'
+                        }
+                    }
+                }
 
-                return res.status(200).send({ response: result});
+                return res.status(201).send({ response: response});
             }
         );
     });
@@ -42,11 +75,21 @@ router.post('/', (req, res, next) => {
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error })}
+                const response = {
+                    message: 'Product created successfully',
+                    createdProduct: {
+                        id_product: result.insertId,
+                        name_product: req.body.name_product,
+                        price_product: req.body.price_product,
+                        request: {
+                            type: 'GET',
+                            description: 'Return all products',
+                            url: 'http://localhost:3000/products'
+                        }
+                    }
+                }
 
-                return res.status(201).send({
-                    message: 'Product created successfully.',
-                    id_product: result.insertId
-                });
+                return res.status(201).send({ response: response});
             }
         );
     });
@@ -69,9 +112,21 @@ router.patch('/', (req, res, next) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error })}
 
-                return res.status(202).send({
-                    message: 'Product updated successfully.'
-                });
+                const response = {
+                    message: 'Product updated successfully',
+                    updatedProduct: {
+                        id_product: req.body.id_product,
+                        name_product: req.body.name_product,
+                        price_product: req.body.price_product,
+                        request: {
+                            type: 'GET',
+                            description: 'Return product details',
+                            url: 'http://localhost:3000/products/' + req.body.id_product
+                        }
+                    }
+                }
+
+                return res.status(202).send({ response: response});
             }
         );
     });
@@ -81,20 +136,29 @@ router.delete('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error })}
         conn.query(
-            `DELETE FROM products WHERE id_product = ?`,
+            'DELETE FROM products WHERE id_product = ?',
             [req.body.id_product],
             (error, result, fields) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error })}
 
-                return res.status(202).send({
-                    message: 'Product deleted successfully.'
-                });
+                const response = {
+                    message: 'Product deleted successfully',
+                    request: {
+                        type: 'POST',
+                        description: 'Create a product',
+                        url: 'http://localhost:3000/products',
+                        body: {
+                            name_product: 'String',
+                            price_product: 'Number'
+                        }
+                    }
+                }
+
+                return res.status(202).send({ response: response});
             }
         );
     });
 });
-
-
 
 module.exports = router;
